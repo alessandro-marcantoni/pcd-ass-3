11b88b7325480178f4bf392c7eb412cff5958cd5
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-06-25 10:17:01 +0200

    First example of simple gui

--- src/main/scala/WordCounter.scala
+++ src/main/scala/WordCounter.scala
@@ -1,6 +1,10 @@
 import Actors.Main
 import Messages._
 import akka.actor.typed.ActorSystem
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.control.Button
 
 import java.io.File
 
@@ -10,6 +14,17 @@ object Messages {
   case class Directory(directory: File) extends Message
   case class Document(file: File) extends Message
   case class WordList(words: List[String]) extends Message
+}
+
+object GUI extends JFXApp {
+  stage = new PrimaryStage {
+    title = "Word Counter"
+    scene = new Scene(800, 600) {
+      val btnStart = new Button("Start")
+      val btnStop = new Button("Stop")
+      content = List(btnStart, btnStop)
+    }
+  }
 }
 
 object WordCounter extends App {
