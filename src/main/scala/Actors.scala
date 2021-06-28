7a6b9c217fc1300f3048cb7d76da2b6fde5ced65
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-06-28 18:56:26 +0200

    Added renderer actor

--- src/main/scala/Actors.scala
+++ src/main/scala/Actors.scala
@@ -49,18 +49,28 @@ object Actors {
   }
 
   object Counter {
-    def apply(ignoredWordsPath: String, nWords: Int): Behavior[Message] = Behaviors.setup(_ => {
+    def apply(ignoredWordsPath: String, nWords: Int, renderer: ActorRef[Message]): Behavior[Message] = Behaviors.setup(_ => {
       val ignoredWords: List[String] = Files.readAllLines(Path.of(ignoredWordsPath), Charset.defaultCharset()).asScala.toList
-      var occurrences: Map[String,Int] = Map()
-      Behaviors.receive { (ctx, msg) => msg match {
+      var occurrences: Map[String, Int] = Map()
+      Behaviors.receiveMessage {
         case WordList(l) =>
           val (alreadyThere, notYet) = l.filter(!ignoredWords.contains(_)).partition(occurrences contains _)
           alreadyThere foreach (w => occurrences = occurrences + (w -> (occurrences(w) + 1)))
           notYet foreach (w => occurrences = occurrences + (w -> 1))
-          ctx.log.info(occurrences.toList.sortWith((a, b) => a._2 > b._2).take(nWords).toString())
+          renderer ! Occurrences(occurrences.toList.sortWith((a, b) => a._2 > b._2).take(nWords))
           Behaviors.same
-      } }
+      }
     })
+  }
+
+  object Renderer {
+    def apply(): Behavior[Message] = Behaviors.receive { (ctx, msg) =>
+      msg match {
+        case Occurrences(occurrences) =>
+          ctx.log.info(occurrences.toString())
+          Behaviors.same
+      }
+    }
   }
 
   object Main {
@@ -74,7 +84,8 @@ object Actors {
     }
 
     def mainBehavior(ctx: ActorContext[Message], parameters: Parameters): Behavior[Message] = {
-      val counter: ActorRef[Message] = ctx.spawn(Counter(parameters.ignoredWordsPath, parameters.nWords), "counter")
+      val renderer: ActorRef[Message] = ctx.spawn(Renderer(), "renderer")
+      val counter: ActorRef[Message] = ctx.spawn(Counter(parameters.ignoredWordsPath, parameters.nWords, renderer), "counter")
       val stripper: ActorRef[Message] = ctx.spawn(Stripper(counter), "stripper")
       val discoverer: ActorRef[Message] = ctx.spawn(Discoverer(stripper), "discoverer")
       discoverer ! Directory(parameters.directory)
