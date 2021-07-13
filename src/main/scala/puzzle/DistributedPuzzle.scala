99a03acf171bfeeed5026f64a7cd1402dcae7df8
Tommaso Mandoloni <tommaso.mandoloni@studio.unibo.it>
2021-07-13 15:43:04 +0200

    add puzzle.conf

--- src/main/scala/puzzle/DistributedPuzzle.scala
+++ src/main/scala/puzzle/DistributedPuzzle.scala
@@ -1,10 +1,32 @@
 package puzzle
 
-object DistributedPuzzle extends App {
+import akka.actor.ActorRef
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
+import com.typesafe.config.ConfigFactory
+
+object DistributedPuzzle {
   val n: Int = 3
   val m: Int = 5
   val imagePath: String = "res/bletchley-park-mansion.jpg"
 
-  val puzzle: PuzzleBoard = PuzzleBoard(n, m, imagePath)
-  puzzle.setVisible(true)
+  def main(args: Array[String]): Unit = {
+    val puzzle: PuzzleBoard = PuzzleBoard(n, m, imagePath)
+    initializeCluster()
+    puzzle.setVisible(true)
+  }
+
+  def initializeCluster(): Unit = {
+    val config = ConfigFactory.load("puzzle")
+    ActorSystem[Nothing](RootBehavior(), "Player", config)
+  }
+
+  object RootBehavior {
+    def apply(): Behavior[Nothing] =
+      Behaviors.setup[Nothing] { context =>
+        context.spawn(behavior, "Child")
+        Behaviors.empty
+      }
+  }
+
 }
